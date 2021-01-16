dc3ec92da21f2a139b1384f7b47079f0b7158494
chetan chumble <chumblechetan@gmail.com>
2021-01-16 16:34:45 +0530

    16-01-2021

--- src/app/pages/survey/survey.component.ts
+++ src/app/pages/survey/survey.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.scss']
 })
 export class SurveyComponent implements OnInit {
-
-  constructor() { }
+  latitude: number;
+  longitude: number;
+  zoom:number;
+  private geoCoder;
+  address: string;
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+  constructor(  private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 15;
+      });
+    }
+  }
+  markerDragEnd($event: MouseEvent) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
   }
 
 }
--- src/app/pages/survey/survey.module.ts
+++ src/app/pages/survey/survey.module.ts
@@ -2,13 +2,16 @@ import { SurveyComponent } from './survey.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SurveyRoutingModule } from './survey-routing.module';
-
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
   declarations: [SurveyComponent],
   imports: [
-    CommonModule, SurveyRoutingModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD0fhyInxQCAVmBPVGJLN_QIF29pJfyYfQ',
+      libraries: ['places']
+    }), CommonModule, SurveyRoutingModule
   ]
 })
 export class SurveyModule { }
