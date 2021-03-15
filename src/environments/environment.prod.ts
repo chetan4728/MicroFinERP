c9d11654b32ba3377e4ff97717efbd44af880bd4
chetan chumble <chumblechetan@gmail.com>
2021-03-15 14:18:29 +0530

    15-03-2021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'https://nisargenterprises.in/BankERP/api/',
+  api: 'https://ally-international.com/portal/api/',
   userSession: 'userSession',
   SessionTime: 1440,
   production: true
