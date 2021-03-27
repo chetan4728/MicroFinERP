b6c9d2f65136757444f49b597addd56441b60ff3
chetan chumble <chumblechetan@gmail.com>
2021-03-27 17:44:41 +0530

    27-03-2021

--- src/app/pages/loan/loan.component.ts
+++ src/app/pages/loan/loan.component.ts
@@ -19,11 +19,21 @@ export class LoanComponent implements OnInit {
   dtTrigger: Subject<String> =  new Subject();
   SessionData: any;
   ListingData:any;
+  BranchList: any;
+  AreaList: any;
+  CenterList: any;
+  GroupList: any;
   Url:any;
+  branch_dp:any ="";
+  area_dp:any="";
+  center_dp:any="";
+  group_dp:any="";
+  filter:any = [];
   constructor(private router: Router, private api: LoanService,private local :LocalStorageService) { }
 
   ngOnInit(): void {
     this.SessionData = this.local.get(environment.userSession);
+    this.loadBranch();
     this.getListing();
     this.Url = environment.uploads;
   }
@@ -42,11 +52,70 @@ export class LoanComponent implements OnInit {
       }
   });
   }
+  loadBranch():void{
+    this.api._get_branch().subscribe(data => {
+      this.BranchList = data;
+      
+    
+  });
+  }
+
+  onChangeBranch(id):void{
+    //alert(id)
+    this.api._get_area({branch_id:id}).subscribe(data => {
+      this.AreaList = data;
+
+    
+      
+  });
+  }
+
+  onChangeArea(id):void{
+    //alert(id)
+    this.api._get_centers({area_id:id}).subscribe(data => {
+      this.CenterList = data;
+
+    
+   
+  });
+  }
+  onChangeCenter(id):void{
+    //alert(id)
+    this.api._get_groups({center_id:id}).subscribe(data => {
+      this.GroupList = data;
+    
+  });
+  }
+  
 
   viewForm(data): void
   {
 
     this.router.navigate(['/loans/LoanForm/' + data.loan_application_no]);
+  }
+
+  filterData(): void
+  {
+    var a = [];
+    if(this.branch_dp!="")
+    {
+      a.push({branch_id:this.branch_dp});
+    }
+    if(this.area_dp!="")
+    {
+      a.push({area_id:this.area_dp});
+    }
+
+    if(this.center_dp!="")
+    {
+      a.push({center_id:this.center_dp});
+    }
+    if(this.center_dp!="")
+    {
+      a.push({center_id:this.center_dp});
+    }
+  
+   alert(JSON.stringify(a))
   }
 
 }
--- src/app/services/loan.service.ts
+++ src/app/services/loan.service.ts
@@ -15,9 +15,13 @@ export class LoanService {
   private REST_API_SERVER = environment.api;
   private GET = this.REST_API_SERVER + 'Loans/LoadTable';
   private GET_SINGLE = this.REST_API_SERVER + 'Loans/get_single_record';
+  private GET_BRANCH_TABLE = this.REST_API_SERVER + 'Branch/LoadTable';
+  private GET_AREA = this.REST_API_SERVER + 'Area/LoadTable';
+  private GET_CENTERS = this.REST_API_SERVER + 'Centers/LoadCentersFromArea';
+  private GET_GROUPS = this.REST_API_SERVER + 'Groups/LoadGroupsFromcenter';
   constructor(private httpClient: HttpClient) { }
 
-
+ 
  
   _get_loans(data): Observable<String>{
     return this.httpClient.post<String>(`${this.GET}`,data).pipe(
@@ -30,7 +34,27 @@ export class LoanService {
       catchError(this.handleError)
     );
   }
- 
+  _get_branch(): Observable<String>{
+    return this.httpClient.get<String>(`${this.GET_BRANCH_TABLE}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+  _get_centers(data): Observable<String>{
+    return this.httpClient.post<String>(`${this.GET_CENTERS}`,data).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  _get_groups(data): Observable<String>{
+    return this.httpClient.post<String>(`${this.GET_GROUPS}`,data).pipe(
+      catchError(this.handleError)
+    );
+  }
+  _get_area(data): Observable<String>{
+    return this.httpClient.post<String>(`${this.GET_AREA}`,data).pipe(
+      catchError(this.handleError)
+    );
+  }
   private handleError <T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
